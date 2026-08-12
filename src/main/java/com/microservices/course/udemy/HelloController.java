cdeb3672f330f05ba49267c05cc4648af8bc4aac
georgiana-stoica <92844869+georgiana-stoica@users.noreply.github.com>
2026-08-12 14:21:45 +0300

    Add missing imports to HelloController
    Added SLF4J Logger for HelloController.

--- src/main/java/com/microservices/course/udemy/HelloController.java
+++ src/main/java/com/microservices/course/udemy/HelloController.java
@@ -3,6 +3,8 @@ package com.microservices.course.udemy;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Locale;
 
