07cd3d24ee0dfe20ae691c1583787c9edf31bb34
georgiana-stoica <92844869+georgiana-stoica@users.noreply.github.com>
2026-08-12 14:34:58 +0300

    Add NoArgsConstructor annotation to User class

--- src/main/java/com/microservices/course/udemy/User.java
+++ src/main/java/com/microservices/course/udemy/User.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
